4f1a1d889b7e7ea225ecd3fa9d6d6ee0af679f30
JANGwonsuk <jjangga700@naver.com>
2022-06-28 18:05:18 +0900

    2022.06.28

--- server/connection.php
+++ server/connection.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$conn = mysqli_connect("localhost","root","","shopping_korea")
+$conn = mysqli_connect("database-1-instance-1.cqywbbvmrjx8.ap-northeast-2.rds.amazonaws.com","admin","wkd961011","shopping_korea")
         or die("Couldn't connect to database");
 
 

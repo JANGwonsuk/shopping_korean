2a9e65fea3cf78e5c8fc31ed221204200a9e06f8
JANGwonsuk <jjangga700@naver.com>
2022-06-28 22:43:03 +0900

    2022.06.28
    22:43

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 
 
-<?php include('layouts/header.php'); ?>
+<?php include('layouts/header.php');?>
 
 
 
--- login.php
+++ login.php
@@ -1,6 +1,4 @@
-<?php include('layouts/header.php'); ?>
-
-
+<?php include('layouts/header.php');?>
 <?php
 
 

637a81f962958720fae6ed12fd341c8eda9af928
JANGwonsuk <jjangga700@naver.com>
2022-06-28 18:23:00 +0900

    Update account.php

--- account.php
+++ account.php
@@ -2,7 +2,7 @@
 <?php
 include('server/connection.php');
 if(!isset($_SESSION['logged_in'])){
-  @header('location: login.php');
+  header('location: login.php');
   exit;
 }
 if(isset($_GET['logout'])){

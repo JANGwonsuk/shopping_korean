d09c51f84d70e2f7d3fd1c8b37a91ad0c420263d
JANGwonsuk <jjangga700@naver.com>
2022-06-29 15:46:46 +0900

    Update connection.php

--- server/connection.php
+++ server/connection.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$conn = mysqli_connect("database-1.cqywbbvmrjx8.ap-northeast-2.rds.amazonaws.com","admin","jjangga700","shopping_korea")
+$conn = mysqli_connect("database-1.cqywbbvmrjx8.ap-northeast-2.rds.amazonaws.com","admin","wkd961011","shopping_korea")
         or die("Couldn't connect to database");
 
 
